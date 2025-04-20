722e6b8a000185c8c4ca4d5b11a812b4edb02008
realelisha <tiongepp@gmail.com>
2025-04-20 21:47:57 +0800

    work branch overview

--- app/src/main/java/com/example/testversion/BranchOverview.kt
+++ app/src/main/java/com/example/testversion/BranchOverview.kt
@@ -2,15 +2,17 @@ package com.example.testversion
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.google.firebase.auth.FirebaseAuth
 
-
 class BranchOverview : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_branch_overview)
@@ -37,7 +39,22 @@ class BranchOverview : AppCompatActivity() {
         bookingButton.setOnClickListener {
             startActivity(Intent(this, BookingActivity::class.java))
         }
+
+        // Click listeners for the 3 cards
+        val mountainCard = findViewById<CardView>(R.id.mountainCard)
+        val islandCard = findViewById<CardView>(R.id.islandCard)
+        val cityCard = findViewById<CardView>(R.id.cityCard)
+
+        mountainCard.setOnClickListener {
+            startActivity(Intent(this, MountainBranch::class.java))
+        }
+
+        islandCard.setOnClickListener {
+            startActivity(Intent(this, IslandBranch::class.java))
+        }
+
+        cityCard.setOnClickListener {
+            startActivity(Intent(this, CityBranch::class.java))
+        }
     }
-
-
 }
---
+++ app/src/main/java/com/example/testversion/MountainBranch.kt
@@ -0,0 +1,12 @@
+package com.example.testversion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MountainBranch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mountain_branch)
+
+    }
+}
