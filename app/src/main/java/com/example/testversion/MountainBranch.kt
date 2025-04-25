232cb70812d6a16331edea8595b5c75a5ab9322b
realelisha <tiongepp@gmail.com>
2025-04-25 21:09:00 +0800

    Add back button for all branch overview

--- app/src/main/java/com/example/testversion/MountainBranch.kt
+++ app/src/main/java/com/example/testversion/MountainBranch.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.testversion.databinding.ActivityMountainBranchBinding
+import android.widget.ImageView
+
 
 class MountainBranch : AppCompatActivity() {
 
     private lateinit var binding: ActivityMountainBranchBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,11 @@ class MountainBranch : AppCompatActivity() {
         Glide.with(this)
             .load(R.drawable.mount3)
             .into(binding.imageView3)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
 
         binding.btnSelectRoom.setOnClickListener {
             startActivity(Intent(this, MountainRoom::class.java))
