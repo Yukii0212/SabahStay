11e4e4bccb4284e068e04beb1f161eb5ba464ef0
realelisha <tiongepp@gmail.com>
2025-04-25 22:45:53 +0800

    .

--- app/src/main/java/com/example/testversion/CityRoom.kt
+++ app/src/main/java/com/example/testversion/CityRoom.kt
@@ -1,6 +1,19 @@
 package com.example.testversion
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class CityRoom : AppCompatActivity(){
+class CityRoom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_room) // Your updated layout file
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
 }
